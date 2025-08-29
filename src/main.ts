73dd8e6c1b678c0d68a1f67d4650970081572be5
IDO37 <charles5275407@gmail.com>
2025-08-29 20:43:51 +0900

    up

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
-import './styles/main.css'
+import './style.css'
 
 // Initialize Pinia with persistence
 const pinia = createPinia()
